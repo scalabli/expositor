b68b56433d312d86869be9581d45116be433ee06
Gerrishon Sirere <s.gerrishon@gmail.com>
2021-06-12 08:20:12 +0300

    Update __init__.py

--- src/expositor/__init__.py
+++ src/expositor/__init__.py
@@ -1,4 +1,31 @@
+"""
+Shodan CLI
 
+Note: Always run "shodan init <api key>" before trying to execute any other command!
 
-from expositor.client import Shodan
-from expositor.exception import APIError
+A simple interface to search Shodan, download data and parse compressed JSON files.
+The following commands are currently supported:
+
+    alert
+    convert
+    count
+    data
+    download
+    honeyscore
+    host
+    info
+    init
+    myip
+    parse
+    radar
+    scan
+    search
+    stats
+    stream
+
+"""
+
+from shodan.client import Shodan
+from shodan.exception import APIError
+
+__version__ = "2021.6.dev1"
